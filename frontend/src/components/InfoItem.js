7388ece2f7e54c41c83967a6259d2d44bf774edf
Purnima Kumari <purnimakumari001@gmail.com>
2025-12-05 00:39:01 +0530

    updated the card icons

--- frontend/src/components/InfoItem.js
+++ frontend/src/components/InfoItem.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const InfoItem = ({item}) => {
+const InfoItem = ({ item }) => {
   return (
-    <>
-      <div className="bg-[#f5f7fa] w-[18rem] sm:w-[22rem] p-4 h-[14rem] my-5 border-solid border-y-2 border-blue-600 flex flex-col  justify-evenly">
-      <Link to={item.link}><h3 className="text-xl font-bold hover:text-blue-500">{item.heading}</h3></Link>
-        <p className="text-[16px]  leading-6">{item.para}</p>
+    <div className="bg-white shadow-md rounded-xl p-6 hover:shadow-lg transition duration-300">
+
+      {/* ICON DISPLAY */}
+      <div className="flex justify-center mb-4">
+        <div className="text-yellow-500 w-10 h-10 flex items-center justify-center">
+          {item.icon}
+        </div>
       </div>
-    </>
+
+      {/* HEADING */}
+      <Link to={item.link}> <h3 className="text-xl font-semibold text-gray-800 hover:text-yellow-500 text-center mb-2">
+        {item.heading}
+      </h3></Link>
+
+      {/* PARAGRAPH */}
+      <p className="text-gray-600 text-center">
+        {item.para}
+      </p>
+
+    </div>
   );
 };
 
