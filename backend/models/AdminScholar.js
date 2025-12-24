7b49fdc973ca884ef68916dd58ee5695ee374df5
5Ankit585 <virendarrawat884@gmail.com>
2025-12-24 18:58:05 +0530

    updated

--- backend/models/AdminScholar.js
+++ backend/models/AdminScholar.js
@@ -18,7 +18,8 @@ const adminScholarSchema = new mongoose.Schema(
     ================================ */
     level: { type: String, trim: true },
     type: [{ type: String }],
-    coveragetype: [{ type: String }],
+    coverageType: { type: String },
+
 
 
     /* ===============================
