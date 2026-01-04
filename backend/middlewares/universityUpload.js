25144cae2bb601bcca28d69bf9d149677873e1fd
5Ankit585 <virendarrawat884@gmail.com>
2026-01-04 22:27:12 +0530

    register .env

--- backend/middlewares/universityUpload.js
+++ backend/middlewares/universityUpload.js
@@ -5,12 +5,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("☁️ CLOUDINARY ENV CHECK:", {
-  name: process.env.CLOUDINARY_CLOUD_NAME,
-  key: process.env.CLOUDINARY_API_KEY,
-  secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
